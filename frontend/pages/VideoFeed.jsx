6f37cd99c9cd763dac8f8bbe2ff57ef37d281830
Ethan Bielecki <ebielecki2003@gmail.com>
2024-09-25 09:55:07 -0400

    Added tailwindcss

--- frontend/pages/VideoFeed.jsx
+++ frontend/pages/VideoFeed.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoFeed = () => {
     return (
-    <div>
+    <div className='flex justify-center items-center min-h-screen'>
         <img src="http://127.0.0.1:5000/video_feed" alt="Video Stream" />
     </div>
     );
